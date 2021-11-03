375b029e398a3514f71ddaeef6a98a577384c6cb
yagolucas2020 <yagolucas21.yl@gmail.com>
2021-11-03 20:55:17 -0300

    Treinando metodo Scanner

---
+++ preta.java
@@ -0,0 +1,37 @@
+package faixa;
+import java.util.Scanner;
+public class preta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String nome, sobrenome;
+		
+	
+		Double rg, cpf;
+		
+		
+		
+		System.out.print("Digite o nome: ");
+		nome = sc.next();
+		
+		
+		System.out.print("Digite o Sobrenome: ");
+		sobrenome = sc.next();
+		
+		
+		
+		System.out.print("Digite o RG: ");
+		 rg = sc.nextDouble();
+		 
+		 
+		 System.out.print("Digite o Cpf: ");
+		 cpf = sc.nextDouble();
+		 
+		 
+	}
+
+}
+
